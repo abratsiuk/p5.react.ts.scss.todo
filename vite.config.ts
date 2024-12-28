210c94f943ca414213a820ca741fb97e206f100e
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-12-28 21:36:07 +0200

    setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import postcssNormalize from 'postcss-normalize';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    historyApiFallback: true, // for React Router
+  },
+  css: {
+    postcss: {
+      plugins: [
+        postcssNormalize(), // for normalize.css
+      ],
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use '/src/styles/variables.scss' as *;`,
+      },
+    },
+  },
+});
