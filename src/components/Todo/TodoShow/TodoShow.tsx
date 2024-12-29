6e05f6996ab3552ffe0ff2a7ebd15035f1daff99
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-12-29 18:54:22 +0200

    Todo finished

--- src/components/Todo/TodoShow/TodoShow.tsx
+++ src/components/Todo/TodoShow/TodoShow.tsx
@@ -13,16 +13,20 @@ export const TodoShow: React.FC<TodoShowProps> = ({
       ? styles.TodoShow__completed_completed
       : styles.TodoShow__completed_active,
   ].join(' ');
+  const textClassName = [
+    styles.TodoShow__text,
+    todo.isCompleted
+      ? styles.TodoShow__text_completed
+      : styles.TodoShow__text_active,
+  ].join(' ');
 
   return (
     <div className={styles.TodoShow}>
       <div
         className={completedClassName}
         onClick={() => onSwitchCompleted(todo.id)}
-      >
-        {todo.isCompleted ? <span>&#10003;</span> : ''}
-      </div>
-      <div className={styles.TodoShow__text}>{todo.text}</div>
+      />
+      <div className={textClassName}>{todo.text}</div>
       <button
         className={styles.TodoShow__delete}
         onClick={() => onDeleteTodo(todo.id)}
