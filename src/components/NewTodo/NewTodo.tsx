dae247738b77ff4668a390c361f05593506979f9
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-12-29 12:29:53 +0200

    ...,formatted

--- src/components/NewTodo/NewTodo.tsx
+++ src/components/NewTodo/NewTodo.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NewTodoProps } from './NewTodoProps';
 import './NewTodo.scss';
 
 export const NewTodo: React.FC<NewTodoProps> = ({ onAddTodo }) => {
+  const [value, setValue] = useState<string>('');
+
   return (
     <div className="NewTodo">
-      <input type="text" name="NewTodo" id="NewTodo" />
-      <button onClick={() => onAddTodo('test' + Math.random())}>
-        Добавить
-      </button>
+      <input
+        className="NewTodo__text"
+        type="text"
+        name="NewTodo"
+        id="NewTodo"
+        placeholder="What needs to be done?"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        onKeyDown={(event) => {
+          if (event.code === 'Enter') {
+            onAddTodo(value);
+          }
+          if (event.code === 'Esc') {
+            setValue('');
+          }
+        }}
+      />
+      <button onClick={() => onAddTodo(value)}>Добавить</button>
     </div>
   );
 };
