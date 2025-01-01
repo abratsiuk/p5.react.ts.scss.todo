cee53e1d12e66826a14b12d5fcc66a3849547c97
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-01-01 15:46:53 +0200

    don	 use useLocalStorageSync

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,11 +5,6 @@ export const Footer = () => {
     <div className="Footer">
       <div className="Footer__instruction">
         <div>Double-click to edit a todo.</div>
-        <div>The data is saved in localStorage.</div>
-        <div>
-          It is possible to edit a todo simultaneously across different browser
-          tabs.
-        </div>
       </div>
       <div className="Footer__copying">© 2024 abratsiuk</div>
       <div className="Footer__thanks">
